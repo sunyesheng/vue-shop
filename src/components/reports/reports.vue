e958b7909690a48061b47da1d7c8bb17b15561e1
sunyesheng <1481633100@qq.com>
2021-07-04 11:49:38 +0800

    echart

--- src/components/reports/reports.vue
+++ src/components/reports/reports.vue
@@ -1,13 +1,54 @@
 <template>
-    <div>
-        <h3>数据统计</h3>
-    </div>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+      <div id="main" style="width: 600px; height: 400px"></div>
+    </el-card>
+  </div>
 </template>
 <script>
+// 导入echarts
+import echarts from 'echarts'
+// 装备盒子
 export default {
-
+  data () {
+    return {}
+  },
+  methods: {},
+  created () { },
+  mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    // 准备配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+    // 展示数据
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  }
 }
 </script>
 <style scoped>
-
 </style>
