be78dc6ab54e78dfb533cab7c9dbbc452318a28a
sunyesheng <1481633100@qq.com>
2021-07-30 13:03:12 +0800

    新增和删除

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-app.use(express.static('./新建文件夹/dist'))
-app.listen(8087, () => {
-  console.log('http://127.0.0.1')
-})
+app.use(express.static('./dist'))
+app.listen(8076)
